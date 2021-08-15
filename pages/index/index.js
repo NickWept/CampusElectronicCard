d80c1943d0f2f49066114ee86ffe91071862252c
NickWept <NickWu@gmail.com>
2021-08-15 17:38:06 +0800

    添加首页提示窗口，修改签约和刷脸按钮，修复功能栏背景不显示问题

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,20 +1,15 @@
 // index.js
-// 获取应用实例
-const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    tipsWindowsFlag: true,
+    facePayFlag: true
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  hipeWindows() {
+    var than = this;
+    than.setData({
+      tipsWindowsFlag: false,
+      facePayFlag: false
     })
   },
   onLoad() {
